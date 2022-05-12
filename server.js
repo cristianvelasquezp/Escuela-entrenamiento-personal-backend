2f7a3553047999aa3dd524187cdb2dd93dac293c
Cristian Velasquez <cristianvelasquezp@gmail.com>
2022-05-11 19:54:36 -0500

    Create and config database

---
+++ server.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config({ path: './config.env' });
+
+const app = require('./app');
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useCreateIndex: true
+  })
+  .then(() => console.log('database connection successful'));
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`App listening in port ${port}...`);
+});
